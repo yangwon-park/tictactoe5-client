5a401abb69837edd35694295654b62297983b6d4
yangwon-park <ywonp94@gmail.com>
2025-09-02 11:30:00 +0900

    :necktie: 초기 UI 완성

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
